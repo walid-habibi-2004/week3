74436760650a47e2b343d670c9be4adecfec6a16
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-27 09:10:56 +0000

    Add fetchData function

--- day_4/exercices/exercices_xp_ninja/exercice 1/commands/fetch.js
+++ day_4/exercices/exercices_xp_ninja/exercice 1/commands/fetch.js
@@ -1,1 +1,14 @@
+import axios from "axios";
 
+export async function fetchData() {
+    try {
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts/1"
+        );
+
+        console.log("📦 Fetched Data:");
+        console.log(response.data);
+    } catch (error) {
+        console.error("❌ Error fetching data:", error.message);
+    }
+}
