2121bb23ab414921b5aa3660bd2678f42eec84f6
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-27 09:10:25 +0000

    Implement greet function
    Add a greet function that uses chalk for styled output.

--- day_4/exercices/exercices_xp_ninja/exercice 1/commands/greet.js
+++ day_4/exercices/exercices_xp_ninja/exercice 1/commands/greet.js
@@ -1,1 +1,9 @@
+import chalk from "chalk";
 
+export function greet(name) {
+    const message = `
+${chalk.blue("🥷 Welcome,")} ${chalk.green.bold(name)}!
+${chalk.yellow("Your ninja utility is ready to serve you!")}
+`;
+    console.log(message);
+}
