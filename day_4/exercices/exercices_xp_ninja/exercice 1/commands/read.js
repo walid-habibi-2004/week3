8f944d0cecbb53984c3e8d1d3aedae00e05f213d
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-27 09:11:24 +0000

    Implement readFile function
    Add a function to read and log file content

--- day_4/exercices/exercices_xp_ninja/exercice 1/commands/read.js
+++ day_4/exercices/exercices_xp_ninja/exercice 1/commands/read.js
@@ -1,1 +1,13 @@
+import fs from "fs";
 
+export function readFile(filePath) {
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            console.error(" Error reading file:", err.message);
+            return;
+        }
+
+        console.log("📄 File Content:");
+        console.log(data);
+    });
+}
