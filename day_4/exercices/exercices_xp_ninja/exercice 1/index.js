fde1e130e1eeb6ecc79d5faa7c54a32379bff63c
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-27 09:12:26 +0000

    Implement CLI commands
    Added CLI commands for greeting, fetching data, and reading files.

--- day_4/exercices/exercices_xp_ninja/exercice 1/index.js
+++ day_4/exercices/exercices_xp_ninja/exercice 1/index.js
@@ -1,1 +1,36 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import { greet } from "./commands/greet.js";
+import { fetchData } from "./commands/fetch.js";
+import { readFile } from "./commands/read.js";
 
+const program = new Command();
+
+program
+    .name("ninja-utility")
+    .description("A powerful ninja CLI utility 🥷")
+    .version("1.0.0");
+
+program
+    .command("greet <name>")
+    .description("Display a colorful greeting")
+    .action((name) => {
+        greet(name);
+    });
+
+
+program
+    .command("fetch")
+    .description("Fetch data from a public API")
+    .action(() => {
+        fetchData();
+    });
+
+program
+    .command("read <file>")
+    .description("Read and display file content")
+    .action((file) => {
+        readFile(file);
+    });
+
+program.parse(process.argv);
