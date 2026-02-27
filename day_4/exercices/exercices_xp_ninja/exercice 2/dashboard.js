17e9a850937d7a7f0ff6fdb798ed012cbb822e0f
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-27 09:19:16 +0000

    Add weather dashboard

--- day_4/exercices/exercices_xp_ninja/exercice 2/dashboard.js
+++ day_4/exercices/exercices_xp_ninja/exercice 2/dashboard.js
@@ -1,1 +1,16 @@
+const readline = require("readline");
+const { getWeather } = require("./weather");
 
+function startDashboard() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("🌍 Enter a city name: ", (city) => {
+        getWeather(city);
+        rl.close();
+    });
+}
+
+module.exports = { startDashboard };
